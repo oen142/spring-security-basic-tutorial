b02445a01c440270be5988d20d689f9e514ce6db
wani <oen142@naver.com>
2020-12-11 17:25:40 +0900

    feat : 구글 로그인 준비

--- src/main/java/com/wani/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/wani/springsecurity/config/SecurityConfig.java
@@ -10,7 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
-@EnableGlobalMethodSecurity(securedEnabled = true , prePostEnabled = true) //secured 어노테이션 활성화 ,  preAuthorize postAuthorize 어노테이션 활성화
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+//secured 어노테이션 활성화 ,  preAuthorize postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -30,6 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login이라는 주소가 호출이되면 시큐리티가 낚아채서 대신 로그인을 진행해줍니다.
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요하다.
     }
 }
